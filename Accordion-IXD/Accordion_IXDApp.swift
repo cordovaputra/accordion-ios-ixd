9756303c1e6634eaa88792dd381b929e53660807
cordovaputra <cordovaputra.handri@gmail.com>
2023-12-12 16:25:23 +0700

    Initial Commit

---
+++ Accordion-IXD/Accordion_IXDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accordion_IXDApp.swift
+//  Accordion-IXD
+//
+//  Created by Cordova  on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Accordion_IXDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
